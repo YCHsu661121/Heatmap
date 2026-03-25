194cdde151c1851a24b5f13c26588086a80e5253
YCHsu <ychsu@ami.com>
2026-03-25 14:32:22 +0800

    chore: add .gitignore, remove out/ dist/ from tracking

--- out/types/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * Stock Heatmap VS Code Extension — 統一型別定義
- * 所有跨模組共用的 TypeScript 介面與型別別名均定義於此。
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=index.js.map
